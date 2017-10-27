2985459b959f40e45dbf8195b035971434b6b444
juslintek <jusys.linas@gmail.com>
2017-10-27 16:26:17 +0300

    Added composer based requirements

--- oauth/login.php
+++ oauth/login.php
@@ -1,10 +1,10 @@
 <?php
 chdir('..');
-require_once('common.inc');
+require_once('../common.inc');
 $user = null;
 
 set_include_path(get_include_path() . PATH_SEPARATOR . './lib/oauth');
-require_once 'Google/Client.php';
+require_once '../Google/Client.php';
 $client_id = GetSetting('google_oauth_client_id');
 $client_secret = GetSetting('google_oauth_client_secret');
 $protocol = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_SSL']) && $_SERVER['HTTP_SSL'] == 'On')) ? 'https' : 'http';
